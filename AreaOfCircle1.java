1887f5098340693cdecd96a38d82a26254f4efed
Dipali0301 <rauldipali03@gmail.com>
2025-02-07 09:45:28 +0530

    Add files via upload

---
+++ AreaOfCircle1.java
@@ -0,0 +1,12 @@
+class AreaOfCircle1 
+{
+	public static void main(String[] args) 
+	{
+		double radius = 5;
+		double area = 22/7*(radius*radius);
+
+		System.out.println("the area of circle is :" +area);
+
+		
+	}
+}
