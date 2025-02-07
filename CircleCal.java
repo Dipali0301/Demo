d5b98f449aa02b364261ffaf91bd455bbca6856e
Dipali0301 <rauldipali03@gmail.com>
2025-02-07 09:48:33 +0530

    Add files via upload

---
+++ CircleCal.java
@@ -0,0 +1,17 @@
+class CircleCal 
+{
+	public static void main(String[] args) 
+	{
+
+	   double radius = 5;
+	   double pi = 22/7;
+
+	   // calculate perimeter and area
+
+	   double perimeter = 2*pi*radius;
+	   double area = pi*(radius*radius);
+
+		System.out.println("The perimeter of circle is : " +perimeter);
+		System.out.println("The area of circle is : " +area);
+	}
+}
